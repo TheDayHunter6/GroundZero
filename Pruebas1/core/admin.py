1df7c25169d0bdcc5e8168fa639dfa95db3ab8c7
TheDayHunter6 <tito190899@outlook.com>
2022-07-03 20:44:06 -0400

    arreglos

--- Pruebas1/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Pinturas, Autor, Categoria
-
-# Register your models here.
-
-admin.site.register(Pinturas)
-admin.site.register(Autor)
-admin.site.register(Categoria)

a6149c003bec4d1891ea4e697a1ed835d1b1f9cb
TheDayHunter6 <tito190899@outlook.com>
2022-07-08 09:33:31 -0400

    Update views.py

--- Groundzero_ET/core/views.py
+++ Groundzero_ET/core/views.py
@@ -1,7 +1,7 @@
 from email import message
 from multiprocessing import context
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404,render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Pinturas
 from .forms import  crearUsuario, subirPintura, modificarPintura, ContactoForm
 from django.contrib.auth.models import User

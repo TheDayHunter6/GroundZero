21c9ca10022ec66465f0080dded11e7469d342f6
TheDayHunter6 <tito190899@outlook.com>
2022-07-08 09:23:50 -0400

    Update admin.py

--- Groundzero_ET/core/admin.py
+++ Groundzero_ET/core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Pinturas, Categoria
+from .models import Pinturas, Categoria, Contacto
 
 # Register your models here.
 
 admin.site.register(Pinturas)
 admin.site.register(Categoria)
+admin.site.register(Contacto)
